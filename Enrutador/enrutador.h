52bd768d97e25950618107449b36bc0eb9c37af7
Coffee0xff_256 <mateo.rodriguez5@udea.edu.co>
2026-05-11 16:42:16 -0500

    clase enrutador asi como modelamiento de un ejemplo practico

--- Enrutador/enrutador.h
+++ Enrutador/enrutador.h
@@ -1,5 +1,6 @@
 #ifndef ENRUTADOR_H
 #define ENRUTADOR_H
+using namespace std;
 #include <string>
 #include <map>
 
@@ -7,8 +8,39 @@ class Enrutador{
 
 public:
 
+    Enrutador(const string &nombre)
+    {
+        this->nombre = nombre;
+    }
+
+    string getNombre() const
+    {
+        return nombre;
+    }
+
+    const map<string,int> &getEnlaces() const
+    {
+        return enlaces;
+    }
+
+    void agregarEnlace(const string &destino, int costo)
+    {
+        enlaces[destino] = costo;
+    }
+
+    void eliminarEnlace(const string &destino)
+    {
+        enlaces.erase(destino);
+    }
+
+    void actualizarCosto(const string &destino, int nuevoCosto)
+    {
+        enlaces[destino] = nuevoCosto;
+    }
+
+
 private:
     string nombre;
     map<string,int> enlaces;
-}
+};
 #endif // ENRUTADOR_H
--- Enrutador/main.cpp
+++ Enrutador/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <enrutador.h>
 using namespace std;
 
 int main()
