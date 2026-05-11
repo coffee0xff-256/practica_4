c414b414047d1492f73ef9e8173d13a9976a48e5
Coffee0xff_256 <mateo.rodriguez5@udea.edu.co>
2026-05-11 16:31:07 -0500

    modelamiento del enrutador asi como primeras classes

---
+++ Enrutador/enrutador.h
@@ -0,0 +1,14 @@
+#ifndef ENRUTADOR_H
+#define ENRUTADOR_H
+#include <string>
+#include <map>
+
+class Enrutador{
+
+public:
+
+private:
+    string nombre;
+    map<string,int> enlaces;
+}
+#endif // ENRUTADOR_H
